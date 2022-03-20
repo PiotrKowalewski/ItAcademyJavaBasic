685191402aa805742ab08ffc959f8ef050ee0b39
PiotrKowalewski <Piotr_2003@mail.ru>
2022-03-20 20:49:09 +0300

    added hm2

---
+++ src/Class2/Lessons2_2.java
@@ -0,0 +1,11 @@
+package Class2;
+
+public class Lessons2_2 { // \Написать метод который суммирует и складывает результат с произведением переменных
+    public static void main (String[] args) { //возращает полученный результат из метода
+            int a = 10;
+            int b = 15;
+            int c = a + b;
+            System.out.println (c+a*b);
+            return;
+                    }
+}
---
+++ src/Class2/Lessons2_3.java
@@ -0,0 +1,12 @@
+package Class2;
+
+public class Lessons2_3 {
+    public static void main (String[] args) { // Следует вывести на экран промежуток времени в часах - неделях
+        double s = 7200;
+        System.out.println("секунд=" +s);
+        System.out.println(" минут=" + s/60);
+        System.out.println("часыов" +s/60/60);
+        System.out.println(" суток=" + s/60/60/24);
+        System.out.println("недель=" +s/60/60/24/7);
+    }
+}
